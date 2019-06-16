6988f54ae2af942bc1e09092a24ee2a3bb230109
Daniel Delgado <dedd1993@gmail.com>
2019-06-16 18:40:41 -0500

    add template for money exchange page

--- src/app/landing/currency-exchange/currency-exchange.component.ts
+++ src/app/landing/currency-exchange/currency-exchange.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-currency-exchange',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency-exchange.component.scss']
 })
 export class CurrencyExchangeComponent implements OnInit {
+  exchangeForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.setValuesFormBuilder();
+  }
+
+  private setValuesFormBuilder() {
+    this.exchangeForm = new FormGroup({
+      dollarInput: new FormControl({ value: '', disabled: false }, Validators.compose([
+        Validators.required,
+        Validators.pattern(/^\d+\.?\d{0,4}$/)
+      ])),
+      euroInput: new FormControl({ value: '', disabled: true })
+    });
   }
 
 }

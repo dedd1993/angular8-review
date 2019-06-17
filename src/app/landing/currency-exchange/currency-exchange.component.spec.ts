be246b415948875ade9fd098b8cee2ed7485d446
Daniel Delgado <dedd1993@gmail.com>
2019-06-17 04:18:26 -0500

    adding component test

--- src/app/landing/currency-exchange/currency-exchange.component.spec.ts
+++ src/app/landing/currency-exchange/currency-exchange.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EMPTY, throwError, of } from 'rxjs';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { SharedModule } from '../../shared/shared.module';
@@ -36,5 +37,65 @@ describe('CurrencyExchangeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#initFormBuilder', () => {
+    it('should create a form with euro and dollar controls', () => {
+      expect(component.exchangeForm.contains('euroInput')).toBeDefined();
+      expect(component.exchangeForm.contains('dollarInput')).toBeDefined();
+    });
+
+    it('should make the euro control required', () => {
+      const control = component.exchangeForm.get('euroInput');
+      control.setValue('');
+      expect(control.valid).toBeFalsy();
+    });
+
+    it('should validate if euro control is a valid number', () => {
+      const control = component.exchangeForm.get('euroInput');
+      control.setValue('345.6789');
+      expect(control.valid).toBeTruthy();
+    });
+
+    it('should validate that euro control is not valid if has more than 4 decimals', () => {
+      const control = component.exchangeForm.get('euroInput');
+      control.setValue('345.678943434');
+      expect(control.valid).toBeFalsy();
+    });
+  });
+
+  describe('#onCalculate', () => {
+    it('should call the service with euro control value parameter to get the dollar value', () => {
+      const spy = spyOn(currencyExchangeService, 'getDollarEquivalent').and.returnValue(EMPTY);
+      const euroValue = component.exchangeForm.get('euroInput').value;
+
+      component.onCalculate();
+      expect(spy).toHaveBeenCalledWith(Number(euroValue));
+    });
+
+    it('should change the dollar control if service response successfully', () => {
+      spyOn(currencyExchangeService, 'getDollarEquivalent').and.returnValue(of(1.120953));
+      const previousValue = component.exchangeForm.get('dollarInput').value;
+
+      component.onCalculate();
+
+      expect(component.exchangeForm.get('dollarInput').value).not.toEqual(previousValue);
+    });
+
+    it('should format the dollar value (0\'000,0000) if service response successfully', () => {
+      spyOn(currencyExchangeService, 'getDollarEquivalent').and.returnValue(of(1121.75559));
+
+      component.onCalculate();
+
+      expect(component.exchangeForm.get('dollarInput').value).toEqual('1,121.7556');
+    });
+
+    it('should reset dollar control if service response with an error', () => {
+      spyOn(currencyExchangeService, 'getDollarEquivalent').and.returnValue(throwError(null));
+
+      component.onCalculate();
+
+      expect(component.exchangeForm.get('dollarInput').value).toBeNull();
+    });
   });
 });
